70c8ea5228ac4566e976be8abeb83b1b3d7364cd
michaellindsay23 <michaelduffielindsay@gmail.com>
2026-01-14 15:16:04 -0500

    move camera

--- src/cube.cpp
+++ src/cube.cpp
@@ -16,22 +16,36 @@
 #include <iostream>
 
 
+// Window settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+// Camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  6.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+
 // Function definitions
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
-  }
+
+  const float cameraSpeed = 0.05f;
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    cameraPos += cameraSpeed * cameraFront;
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    cameraPos -= cameraSpeed * cameraFront;
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
-
-
-// Window settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
 
 
 // Main display loop
@@ -113,7 +127,7 @@ int main() {
      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-};
+  };
 
   unsigned int indices[] = {
     0, 1, 3,  // first triangle
@@ -164,6 +178,17 @@ int main() {
   glBindVertexArray(0);
 
 
+  // Camera
+  /*glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 6.0f);
+  glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraPos));
+  glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+  glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);*/
+
+
   // Runtime loop
   while (!glfwWindowShouldClose(window)) {
     // Input
@@ -178,10 +203,9 @@ int main() {
 
     // 3D RENDERING
     glm::mat4 model      = glm::mat4(1.0f);
-    glm::mat4 view       = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
     model      = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-    view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
     projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
     unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
